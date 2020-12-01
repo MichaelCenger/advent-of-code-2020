ad3b7ba1e5690f3f3ea611d619d230b64b410829
Michael Cenger <MichaelCenger@gmail.com>
2020-12-01 11:41:37 +0100

    Finish day 1

---
+++ 01_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _01_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> input = ReadInput();
+			for (int i = 0; i < input.Count; i++)
+			{
+				for (int j = i + 1; j < input.Count; j++)
+				{
+					int sum = input[i] + input[j];
+					if (sum == 2020)
+					{
+						int result = input[i] * input[j];
+						Console.WriteLine($"The result is: {result}");
+						return;
+					}
+				}
+			}
+		}
+
+		private static List<int> ReadInput()
+		{
+			List<int> input = new List<int>();
+			string[] lines = File.ReadAllLines(@"input.txt");
+
+			foreach (string line in lines)
+			{
+				input.Add(Convert.ToInt32(line));
+			}
+			return input;
+		}
+	}
+}
---
+++ 01_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _01_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> input = ReadInput();
+			for (int i = 0; i < input.Count; i++)
+			{
+				for (int j = i + 1; j < input.Count; j++)
+				{
+					int sum = input[i] + input[j];
+					//Would be faster with a dictionary indexed on the remains
+					for (int k = 0; k < input.Count; k++)
+					{
+						if (k == j || k == i)
+						{
+							continue;
+						}
+						if (sum + input[k] == 2020)
+						{
+							int result = input[i] * input[j] * input[k];
+							Console.WriteLine($"The result is: {result}");
+							return;
+						}
+					}
+				}
+			}
+
+
+		}
+
+		private static List<int> ReadInput()
+		{
+			List<int> input = new List<int>();
+			string[] lines = File.ReadAllLines(@"input.txt");
+
+			foreach (string line in lines)
+			{
+				input.Add(Convert.ToInt32(line));
+			}
+			return input;
+		}
+	}
+}
