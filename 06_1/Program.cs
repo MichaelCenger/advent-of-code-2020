d1079e72f97447d888eb6ca12009ad21ee271ec0
Michael Cenger <MichaelCenger@gmail.com>
2020-12-06 06:27:26 +0100

    Day 6

---
+++ 06_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _06_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> input = ReadInput();
+			Dictionary<char, bool> possibleValues = new Dictionary<char, bool>();
+			ResetDictionary(possibleValues);
+			int sum = 0;
+			List<string> currentGroup = new List<string>();
+			for (int i = 0; i < input.Count; i++) {
+				if (!string.IsNullOrEmpty(input[i]))
+				{
+					currentGroup.Add(input[i]);
+				}
+
+				if (string.IsNullOrEmpty(input[i]) || i == input.Count - 1)
+				{
+					foreach (string groupEntry in currentGroup)
+					{
+						foreach (char character in groupEntry)
+						{
+							possibleValues[character] = true;
+						}
+					}
+					sum += possibleValues.Where(val => val.Value == true).ToList().Count;
+					ResetDictionary(possibleValues);
+					currentGroup.Clear();
+				}
+			}
+
+			Console.WriteLine(sum);
+		}
+
+		private static void ResetDictionary(Dictionary<char, bool> dictionary)
+		{
+			for (int i = 97; i <= 122; i++)
+			{
+				dictionary[((char)i)] = false;
+			}
+		}
+
+		private static List<string> ReadInput()
+		{
+			List<string> input = new List<string>();
+			string[] lines = File.ReadAllLines(@"input.txt");
+
+			foreach (string line in lines)
+			{
+				input.Add(line);
+			}
+			return input;
+		}
+	}
+}
