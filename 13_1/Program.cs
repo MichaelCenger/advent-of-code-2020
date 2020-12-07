b54af89df51cb6e49ba9ba8838fb05706018d66c
Michael Cenger <MichaelCenger@gmail.com>
2020-12-07 08:46:34 +0100

    Create templates

---
+++ 13_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _13_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> input = ReadInput();
+		}
+
+		private static List<string> ReadInput()
+		{
+			List<string> input = new List<string>();
+			string[] lines = File.ReadAllLines(@"input.txt");
+
+			foreach (string line in lines)
+			{
+				input.Add(line);
+			}
+			return input;
+		}
+	}
+}
