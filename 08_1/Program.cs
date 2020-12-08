1b6eff9ac0b325a8744c65fadb9b9ede6b0f095b
Michael Cenger <MichaelCenger@gmail.com>
2020-12-08 07:15:24 +0100

    Day 8

--- 08_1/Program.cs
+++ 08_1/Program.cs
@@ -9,6 +9,57 @@ namespace _08_1
 		static void Main(string[] args)
 		{
 			List<string> input = ReadInput();
+			Dictionary<int, bool> executed = new Dictionary<int, bool>();
+			for (int i = 0; i < input.Count; i++)
+			{
+				executed[i] = false;
+			}
+			int acc = 0;
+			int j = 0;
+			while(true)
+			{
+				if (executed[j] == true)
+				{
+					break;
+				}
+				if (input[j].Contains("nop"))
+				{
+					j++;
+					continue;
+				}
+
+				executed[j] = true;
+				string operation = input[j].Substring(4, input[j].Length - 4);
+				char sign = operation[0];
+				int value = Convert.ToInt32(operation.Substring(1, operation.Length - 1));
+				if (input[j].Contains("acc"))
+				{
+					switch (sign)
+					{
+						case '+':
+							acc += value;
+							
+							break;
+						case '-':
+							acc -= value;
+							break;
+					}
+					j++;
+				}
+				else if (input[j].Contains("jmp"))
+				{
+					switch (sign)
+					{
+						case '+':
+							j += value;
+							break;
+						case '-':
+							j -= value;
+							break;
+					}
+				}
+			}
+			Console.WriteLine(acc);
 		}
 
 		private static List<string> ReadInput()
--- 08_2/Program.cs
+++ 08_2/Program.cs
@@ -9,6 +9,83 @@ namespace _08_2
 		static void Main(string[] args)
 		{
 			List<string> input = ReadInput();
+			Dictionary<int, List<string>> permutations = new Dictionary<int, List<string>>();
+			for (int i = 0; i < input.Count; i++)
+			{
+				permutations[i] = new List<string>();
+				for (int k = 0; k < input.Count; k++)
+				{
+					permutations[i].Add(input[k]);
+				}
+				string instruction = permutations[i][i];
+				string[] splitString = instruction.Split(' ');
+				if (instruction.Contains("jmp"))
+				{
+					string newInstruction = "nop " + splitString[1];
+					permutations[i][i] = newInstruction;
+				}
+				else if (instruction.Contains("nop"))
+				{
+					string newInstruction = "jmp " + splitString[1];
+					permutations[i][i] = newInstruction;
+				}
+			}
+
+			for (int i = 0; i < permutations.Count; i++)
+			{
+				HashSet<int> visited = new HashSet<int>();
+				int acc = 0;
+				List<string> currentInput = permutations[i];
+
+				int j = 0;
+				while (true)
+				{
+					if (visited.Contains(j))
+					{
+						break;
+					}
+					visited.Add(j);
+					if (j >= currentInput.Count)
+					{
+						Console.WriteLine(acc);
+						break;
+					}
+					if (currentInput[j].Contains("nop"))
+					{
+						j++;
+						continue;
+					}
+					string operation = currentInput[j].Substring(4, currentInput[j].Length - 4);
+					char sign = operation[0];
+					int value = Convert.ToInt32(operation.Substring(1, operation.Length - 1));
+					if (currentInput[j].Contains("acc"))
+					{
+						switch (sign)
+						{
+							case '+':
+								acc += value;
+
+								break;
+							case '-':
+								acc -= value;
+								break;
+						}
+						j++;
+					}
+					else if (currentInput[j].Contains("jmp"))
+					{
+						switch (sign)
+						{
+							case '+':
+								j += value;
+								break;
+							case '-':
+								j -= value;
+								break;
+						}
+					}
+				}
+			}
 		}
 
 		private static List<string> ReadInput()
