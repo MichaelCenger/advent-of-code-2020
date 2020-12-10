2a931d47acb168444212fcc795f3b155e9c14cc3
Michael Cenger <MichaelCenger@gmail.com>
2020-12-10 08:23:07 +0100

    Day 10

--- 10_1/Program.cs
+++ 10_1/Program.cs
@@ -8,17 +8,43 @@ namespace _10_1
 	{
 		static void Main(string[] args)
 		{
-			List<string> input = ReadInput();
+			List<int> input = ReadInput();
+			int oneJoltDiff = 0;
+			int threeJoltDiff = 0;
+			input.Add(0);
+			input.Sort();
+			if (input[0] == 1)
+			{
+				oneJoltDiff++;
+			}
+			else if (input[0] == 3)
+			{
+				threeJoltDiff++;
+			}
+			for (int i = 0; i < input.Count - 1; i++)
+			{
+				if (input[i + 1] - input[i] == 1)
+				{
+					oneJoltDiff++;
+				}
+				else if (input[i + 1] - input[i] == 3)
+				{
+					threeJoltDiff++;
+				}
+			}
+			threeJoltDiff++;
+			Console.WriteLine(oneJoltDiff * threeJoltDiff);
 		}
 
-		private static List<string> ReadInput()
+		private static List<int> ReadInput()
 		{
-			List<string> input = new List<string>();
+			List<int> input = new List<int>();
 			string[] lines = File.ReadAllLines(@"input.txt");
 
 			foreach (string line in lines)
 			{
-				input.Add(line);
+				if (line != "")
+					input.Add(Convert.ToInt32(line));
 			}
 			return input;
 		}
--- 10_2/Program.cs
+++ 10_2/Program.cs
@@ -6,19 +6,89 @@ namespace _10_2
 {
 	class Program
 	{
+		static int count = 0;
+		static int target;
 		static void Main(string[] args)
 		{
-			List<string> input = ReadInput();
+			List<int> input = ReadInput();
+			target = input[input.Count - 1] + 3;
+			input.Add(0);
+			input.Sort();
+			Console.WriteLine(GetPossibleWays(input));
 		}
 
-		private static List<string> ReadInput()
+		private static void CountArrangements(List<int> input, int startIndex)
 		{
-			List<string> input = new List<string>();
+			for (int i = startIndex; i < input.Count; i++)
+			{
+				int possibleAdapterCount = 0;
+				List<int> newList = new List<int>(input);
+				for (int j = 1; j <= Math.Min(3, input.Count - i - 1); j++)
+				{
+					if (input[i + j] - input[i] <= 3)
+					{
+						possibleAdapterCount++;
+						if (possibleAdapterCount > 1)
+						{
+							newList.RemoveAt(i + j - 1);
+							CountArrangements(newList, i);
+						}
+					}
+				}
+			}
+			if (CheckValidity(input))
+			{
+				count++;
+			}
+		}
+
+		private static long GetPossibleWays(List<int> input)
+		{
+			Dictionary<int, long> waysToConnect = new Dictionary<int, long>();
+			//Last element has 1 way to connect
+			waysToConnect[input.Count - 1] = 1;
+
+			for (int i = input.Count - 2; i >= 0; i--)
+			{
+				long possibleAdapterCount = 0;
+				for (int j = i + 1; j < Math.Min(i + 4, input.Count); j++)
+				{
+					if (input[j] - input[i] <= 3)
+					{
+						possibleAdapterCount += waysToConnect[j];
+					}
+				}
+				waysToConnect[i] = possibleAdapterCount;
+			}
+
+			return waysToConnect[0];
+		}
+
+		private static bool CheckValidity(List<int> input)
+		{
+			List<int> copy = new List<int>(input);
+			copy.Add(0);
+			copy.Add(target);
+			copy.Sort();
+			for (int i = 0; i < copy.Count - 1; i++)
+			{
+				if (copy[i + 1] - copy[i] > 3)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private static List<int> ReadInput()
+		{
+			List<int> input = new List<int>();
 			string[] lines = File.ReadAllLines(@"input.txt");
 
 			foreach (string line in lines)
 			{
-				input.Add(line);
+				if (line != "")
+					input.Add(Convert.ToInt32(line));
 			}
 			return input;
 		}
