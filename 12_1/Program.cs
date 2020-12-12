ea511ff2c6b0d74a242f5c853d5040fc8198fedf
Michael Cenger <MichaelCenger@gmail.com>
2020-12-12 07:42:02 +0100

    Day 12

--- 12_1/Program.cs
+++ 12_1/Program.cs
@@ -6,9 +6,67 @@ namespace _12_1
 {
 	class Program
 	{
+		enum Facing
+		{
+			East = 0,
+			South = 90,
+			West = 180,
+			North = 270
+		}
 		static void Main(string[] args)
 		{
 			List<string> input = ReadInput();
+			int x = 0;
+			int y = 0;
+			int rotation = 0;
+			Facing CurrentFacing = Facing.East;
+			for (int i = 0; i < input.Count; i++)
+			{
+				char command = input[i][0];
+				int value = Convert.ToInt32(input[i].Substring(1, input[i].Length - 1));
+				switch (command)
+				{
+					case 'N':
+						y += value;
+						break;
+					case 'E':
+						x += value;
+						break;
+					case 'S':
+						y -= value;
+						break;
+					case 'W':
+						x -= value;
+						break;
+					case 'R':
+						rotation = (rotation + value) % 360;
+						CurrentFacing = (Facing)rotation;
+						break;
+					case 'L':
+						rotation = (360+(rotation - value))%360;
+						CurrentFacing = (Facing)rotation;
+						break;
+					case 'F':
+						switch (CurrentFacing)
+						{
+							case Facing.East:
+								x += value;
+								break;
+							case Facing.South:
+								y -= value;
+								break;
+							case Facing.West:
+								x -= value;
+								break;
+							case Facing.North:
+								y += value;
+								break;
+						}
+						break;
+				}
+			}
+
+			Console.WriteLine(Math.Abs(x) + Math.Abs(y));
 		}
 
 		private static List<string> ReadInput()
--- 12_2/Program.cs
+++ 12_2/Program.cs
@@ -6,9 +6,73 @@ namespace _12_2
 {
 	class Program
 	{
+		enum Facing
+		{
+			East = 0,
+			South = 90,
+			West = 180,
+			North = 270
+		}
 		static void Main(string[] args)
 		{
 			List<string> input = ReadInput();
+			int waypointX = 10;
+			int waypointY = 1;
+			int x = 0;
+			int y = 0;
+			Facing CurrentFacing = Facing.East;
+			for (int i = 0; i < input.Count; i++)
+			{
+				char command = input[i][0];
+				int value = Convert.ToInt32(input[i].Substring(1, input[i].Length - 1));
+				switch (command)
+				{
+					case 'N':
+						waypointY += value;
+						break;
+					case 'E':
+						waypointX += value;
+						break;
+					case 'S':
+						waypointY -= value;
+						break;
+					case 'W':
+						waypointX -= value;
+						break;
+					case 'R':
+					case 'L':
+						if (command == 'L')
+						{
+							value = 360 - value;
+						}
+						if (value == 90)
+						{
+							int startX = waypointX;
+							waypointX = waypointY;
+							waypointY = -startX;
+						}
+
+						if (value == 180)
+						{
+							waypointX = -waypointX;
+							waypointY = -waypointY;
+						}
+
+						if (value == 270)
+						{
+							int startX = waypointX;
+							waypointX = -waypointY;
+							waypointY = startX;
+						}
+						break;
+					case 'F':
+						x += waypointX * value;
+						y += waypointY * value;
+						break;
+				}
+			}
+
+			Console.WriteLine(Math.Abs(x) + Math.Abs(y));
 		}
 
 		private static List<string> ReadInput()
