ee10eee8265041238482a220731ca62151bbb27a
Michael Cenger <MichaelCenger@gmail.com>
2020-12-13 07:49:06 +0100

    Day 13

--- 13_1/Program.cs
+++ 13_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.IO;
 
 namespace _13_1
@@ -9,6 +10,38 @@ namespace _13_1
 		static void Main(string[] args)
 		{
 			List<string> input = ReadInput();
+			float timeToWait = int.MaxValue;
+			int busId = 0;
+
+			int time = Convert.ToInt32(input[0]);
+
+			var lines = input[1].Split(',').ToList();
+
+			lines.RemoveAll(line => { return line == "x"; });
+			Dictionary<int, List<int>> times = new Dictionary<int, List<int>>();
+
+			for (int i = 0; i < lines.Count; i++)
+			{
+				int lineNumber = Convert.ToInt32(lines[i]);
+				int j = 0;
+				times[lineNumber] = new List<int>();
+				while (j* lineNumber < time)
+				{
+					j++;
+					times[lineNumber].Add(lineNumber * j);
+				}
+			}
+
+			foreach(var kvp in times)
+			{
+				if ( kvp.Value[kvp.Value.Count - 1] -time < timeToWait)
+				{
+					timeToWait = kvp.Value[kvp.Value.Count - 1]-time ;
+					busId = kvp.Key;
+				}
+			}
+
+			Console.WriteLine((int)timeToWait * busId);
 		}
 
 		private static List<string> ReadInput()
--- 13_2/Program.cs
+++ 13_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _13_2
 {
@@ -9,6 +10,46 @@ namespace _13_2
 		static void Main(string[] args)
 		{
 			List<string> input = ReadInput();
+
+			var lines = input[1].Split(',').ToList();
+
+			List<int> numbers = new List<int>();
+			List<int> remainders = new List<int>();
+
+			for (int i = 0; i < lines.Count; i++)
+			{
+				if (lines[i] != "x")
+				{
+					numbers.Add(Convert.ToInt32(lines[i]));
+					remainders.Add(i);
+				}
+			}
+
+			int firstTimeStamp = Convert.ToInt32(numbers[0]);
+			long currentTime = 0;
+			long k = 0;
+			//I <3 brute force
+			while (true)
+			{
+				k++;
+				currentTime = k * firstTimeStamp;
+				bool valid = true;
+
+				for (int j = 1; j < numbers.Count; j++)
+				{
+					if (((currentTime + remainders[j]) % numbers[j]) != 0)
+					{
+						valid = false;
+						break;
+					}
+				}
+
+				if (valid)
+				{
+					break;
+				}
+			}
+			Console.WriteLine(currentTime);
 		}
 
 		private static List<string> ReadInput()
