77217f1ad8d430ef5997af8d113e95f9798c9fa8
Michael Cenger <MichaelCenger@gmail.com>
2020-12-15 10:36:18 +0100

    Day 15

--- 15_2/Program.cs
+++ 15_2/Program.cs
@@ -8,17 +8,49 @@ namespace _15_2
 	{
 		static void Main(string[] args)
 		{
-			List<string> input = ReadInput();
+			List<int> input = ReadInput();
+			long lastSpokenNumber = -1;
+			Dictionary<long, long> lastSpoken = new Dictionary<long, long>();
+			Dictionary<long, long> lastSpokenBefore = new Dictionary<long, long>();
+			long currentTurn = 1;
+			for (int i = 0; i < input.Count; i++)
+			{
+				lastSpoken[input[i]] = currentTurn;
+				currentTurn++;
+				lastSpokenNumber = input[i];
+			}
+
+			while (currentTurn != 30000001)
+			{
+				long numberToSpeak = -1;
+				if (lastSpokenBefore.ContainsKey(lastSpokenNumber))
+				{
+					numberToSpeak = lastSpoken[lastSpokenNumber] - lastSpokenBefore[lastSpokenNumber];
+				}
+				else
+				{
+					numberToSpeak = 0;
+				}
+				if (lastSpoken.ContainsKey(numberToSpeak))
+				{
+					lastSpokenBefore[numberToSpeak] = lastSpoken[numberToSpeak];
+				}
+				lastSpoken[numberToSpeak] = currentTurn;
+				lastSpokenNumber = numberToSpeak;
+				currentTurn++;
+
+			}
+			Console.WriteLine(lastSpokenNumber);
 		}
 
-		private static List<string> ReadInput()
+		private static List<int> ReadInput()
 		{
-			List<string> input = new List<string>();
+			List<int> input = new List<int>();
 			string[] lines = File.ReadAllLines(@"input.txt");
-
-			foreach (string line in lines)
+			var split = lines[0].Split(",");
+			foreach (string number in split)
 			{
-				input.Add(line);
+				input.Add(Convert.ToInt32(number));
 			}
 			return input;
 		}
