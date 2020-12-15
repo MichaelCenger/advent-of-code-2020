918db42a6720e074cd8922244040f3e32e7b04a0
Michael Cenger <MichaelCenger@gmail.com>
2020-12-15 10:36:31 +0100

    Add project templates

---
+++ 16_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _16_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> input = ReadInput();
+			for(int i = 0; i < input.Count; i++)
+			{
+
+			}
+		}
+
+		private static List<string> ReadInput()
+		{
+			List<string> input = new List<string>();
+			string[] lines = File.ReadAllLines(@"input.txt");
+
+			foreach (string line in lines)
+			{
+				input.Add(line);
+			}
+			return input;
+		}
+	}
+}
