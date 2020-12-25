b8d61fc576058c75d6517cc3e3b986c601b71278
Michael Cenger <MichaelCenger@gmail.com>
2020-12-25 07:04:33 +0100

    Day 25

--- 25_1/Program.cs
+++ 25_1/Program.cs
@@ -9,6 +9,49 @@ namespace _25_1
 		static void Main(string[] args)
 		{
 			List<string> input = ReadInput();
+			long cardPublicKey = Convert.ToInt64(input[0]);
+
+			int subjectNumber = 7;
+
+			long cardValue = 1;
+			int cardLoopSize = 0;
+			while (cardValue != cardPublicKey)
+			{
+				cardValue *= subjectNumber;
+				cardValue = cardValue % 20201227;
+				cardLoopSize++;
+			}
+			Console.WriteLine(cardLoopSize);
+
+			long doorPublicKey = Convert.ToInt64(input[1]);
+			long doorValue = 1;
+			int doorLoopSize = 0;
+
+			while (doorValue != doorPublicKey)
+			{
+				doorValue *= subjectNumber;
+				doorValue = doorValue % 20201227;
+				doorLoopSize++;
+			}
+			Console.WriteLine(doorLoopSize);
+
+			cardValue = 1;
+			for(int i = 0; i < cardLoopSize; i++)
+			{
+				cardValue *= doorPublicKey;
+				cardValue = cardValue % 20201227;
+			}
+
+			Console.WriteLine(cardValue);
+
+			doorValue = 1;
+			for (int i = 0; i < doorLoopSize; i++)
+			{
+				doorValue *= cardPublicKey;
+				doorValue = doorValue % 20201227;
+			}
+
+			Console.WriteLine(doorValue);
 		}
 
 		private static List<string> ReadInput()
